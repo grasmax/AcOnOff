f25615ba2e6f314b30d9f222a9a3c8c05ba1070d
rh privat <shop5@grasmax.de>
2023-09-15 11:06:33 +0200

    Weitere Testscripts

---
+++ script/gh_mpIIAcOnOff.sh
@@ -0,0 +1,29 @@
+# Unter Windows10 konnte man den SSH-Schluessel dauerhaft mit ssh-add hinterlegen.
+# Unter raspberyy pi os  muss man den  Schluessel bei einem ssh-agent registrieren.
+# An den vom System gestarteten Agent kommt man aber nicht ran.
+# Deshalb wird hier vor der Ausfuehrung des Python-Scripts ein weiterer Agent gestartet und nach der Ausfuehrung wieder entfernt.
+
+date   > /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+echo 'Starte mpIIaconoff.sh '  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
+
+# Einen neuen Agent starten:
+eval "$(ssh-agent -s)" >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
+# Den ssh-Schluessel hinterlegen:
+/usr/bin/ssh-add ~/.ssh/k4  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
+# ssh-Variabelen ins Log schreiben:
+env | grep SSH  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
+# Man koennte nun auch hier DBUS-Werte abfragen.
+# Da das nun aber auch im Python funktioniert, steht das hier nur noch zur Info:
+# /usr/bin/ssh  root@192.168.2.38 "dbus -y com.victronenergy.system /Dc/Battery/Soc GetValue" > /mnt/wd2tb/script/mpIIaconoff/temp/soc.txt
+
+# Rechnen und Schalten:
+cd /mnt/wd2tb/script/mpIIaconoff
+/usr/bin/python /mnt/wd2tb/script/mpIIaconoff/mpIIAcOnOff.py >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
+# Den Agenten wieder beenden:
+eval "$(ssh-agent -k)"  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+
