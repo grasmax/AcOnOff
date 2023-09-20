eb8f7450dcf2be7b4dd613fdd409d5d76e53b8dc
rh privat <shop5@grasmax.de>
2023-09-20 11:25:29 +0200

    Logdatei hat nun einen Zeitstempel im Dateinamen, um dem 23:55-Ausfall aufklären zu können

--- script/gh_mpIIAcOnOff.sh
+++ script/gh_mpIIAcOnOff.sh
@@ -3,18 +3,20 @@
 # An den vom System gestarteten Agent kommt man aber nicht ran.
 # Deshalb wird hier vor der Ausfuehrung des Python-Scripts ein weiterer Agent gestartet und nach der Ausfuehrung wieder entfernt.
 
-date   > /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
-echo 'Starte mpIIaconoff.sh '  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+logfiletimestamp=$(date "+%Y%m%d_%H%M%S")
+
+date   > /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
+echo 'Starte mpIIaconoff.sh '  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
 
 # Einen neuen Agent starten:
-eval "$(ssh-agent -s)" >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+eval "$(ssh-agent -s)" >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
 # Den ssh-Schluessel hinterlegen:
-/usr/bin/ssh-add ~/.ssh/k4  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+/usr/bin/ssh-add ~/.ssh/k4  >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
 # ssh-Variabelen ins Log schreiben:
-env | grep SSH  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+env | grep SSH  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
 # Man koennte nun auch hier DBUS-Werte abfragen.
 # Da das nun aber auch im Python funktioniert, steht das hier nur noch zur Info:
@@ -22,8 +24,8 @@ env | grep SSH  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
 
 # Rechnen und Schalten:
 cd /mnt/wd2tb/script/mpIIaconoff
-/usr/bin/python /mnt/wd2tb/script/mpIIaconoff/mpIIAcOnOff.py >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+/usr/bin/python /mnt/wd2tb/script/mpIIaconoff/mpIIAcOnOff.py >> /mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
 # Den Agenten wieder beenden:
-eval "$(ssh-agent -k)"  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog.txt
+eval "$(ssh-agent -k)"  >>/mnt/wd2tb/script/mpIIaconoff/log/mpIIaconoff_shlog_$logfiletimestamp.txt
 
